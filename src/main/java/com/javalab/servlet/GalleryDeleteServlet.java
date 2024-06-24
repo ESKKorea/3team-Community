319ad1b46a94fe7d1dd0e1b52b9fdc787febdd30
loismeow <sb02085@gmail.com>
2024-06-24 20:08:49 +0900

    갤러리 삭제 기능 추가

---
+++ src/main/java/com/javalab/servlet/GalleryDeleteServlet.java
@@ -0,0 +1,55 @@
+package com.javalab.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalab.dao.GalleryDAO;
+
+/**
+ * Servlet implementation class GalleryDeleteServlet
+ */
+@WebServlet("/galleryDelete")
+public class GalleryDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GalleryDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 파라미터 인코딩
+				request.setCharacterEncoding("utf-8");
+				
+				// 삭제할 게시물 번호 추출
+				int bno = Integer.parseInt(request.getParameter("bno"));
+				
+				// DAO에 게시물 삭제 요청
+				GalleryDAO galleryDAO = GalleryDAO.getInstance();
+				
+				int row = galleryDAO.deleteGallery(bno);
+				
+				// 데이터베이스 작업 결과로 분기
+				if(row > 0) {
+					String contextPath = request.getContextPath();
+					response.sendRedirect(contextPath + "/galleryList");
+				}else {
+					request.setAttribute("error", "게시물 삭제에 실패했습니다.");
+					RequestDispatcher rd = request.getRequestDispatcher("/galleryDetail?bno=" + bno);
+					rd.forward(request, response);
+				}
+			}
+
+		}
